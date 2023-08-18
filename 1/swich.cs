78dcb1984ea3e9e4eb8726a71c4a07cc936fcf6e
Antonina66 <88768298+Antonina66@users.noreply.github.com>
2023-08-19 00:08:42 +0300

    commit-11

---
+++ 1/swich.cs
@@ -0,0 +1,23 @@
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+switch (number%10) 
+{
+    case 1: 
+    Console.WriteLine("один");
+    break;
+
+    case 2: 
+    Console.WriteLine("два");
+    break;
+
+    case 3: 
+    Console.WriteLine("три");
+    break;
+
+    case 4: 
+    Console.WriteLine("четыре");
+    break;
+}
+
