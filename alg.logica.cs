90185580919aefb18393dc6fdd9121b676bdccd3
Antonina66 <88768298+Antonina66@users.noreply.github.com>
2023-08-18 22:57:00 +0300

    commit-9

--- alg.logica.cs
+++ alg.logica.cs
@@ -83,3 +83,28 @@ if (a > b && b > c)
 {
 	Console.WriteLine("{0}, {1}, {2}", a, b, c);
 }
+
+if (a < b && b < c)
+{
+	Console.WriteLine("{0}, {1}, {2}",  c, b, a);
+}
+
+if (c > a && a > b)
+{
+	Console.WriteLine("{0}, {1}, {2}", c, a, b);
+}
+
+if (b > a && a > c)
+{
+	Console.WriteLine("{0}, {1}, {2}", b, a, c);
+}
+
+if (b > c && c > a)
+{
+	Console.WriteLine("{0}, {1}, {2}", b, c, a);
+}
+
+if (a > c && c > b)
+{
+	Console.WriteLine("{0}, {1}, {2}", a, c, b);
+}
